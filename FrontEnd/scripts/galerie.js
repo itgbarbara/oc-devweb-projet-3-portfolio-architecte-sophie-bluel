0508122b6be076f084f61b9e090ec9f751b23ccc
itgbarbara <itgbarbara@gmail.com>
2024-05-15 10:38:39 +0200

    Storing data in the localStorage

--- FrontEnd/scripts/galerie.js
+++ FrontEnd/scripts/galerie.js
@@ -1,13 +1,29 @@
 /*
-** Import des fonctions du fichier script.js
+** Import des fonctions
 **/
 
 
 /*
-** Récupération des données depuis l'API HTTP
+** Récupération des données depuis le localStorage ou l'API HTTP
 */
-const reponse = await fetch("http://localhost:5678/api/works") // Envoi d'une requête pour récupérer les données de l'API (route : GET /works)
-const travaux = await reponse.json() // Désérialisation du JSON et stockage de la liste d'objets (= travaux) obtenue dans la variable "travaux"
+//** Récupération des pièces éventuellement stockées dans le localStorage **//
+let travaux = window.localStorage.getItem("travaux")
+if (travaux === null) {
+    //** S'il n'y a rien dans le localStorage, récupération des travaux depuis l'API **//
+    const reponse = await fetch("http://localhost:5678/api/works") // Envoi d'une requête pour récupérer les données de l'API (route : GET /works)
+    const travaux = await reponse.json() // Désérialisation du JSON et stockage de la liste d'objets (= travaux) obtenue dans la variable "travaux"
+
+    //** Transformation des travaux en JSON pour les stocker dans le localStorage **//
+    const valeurTravaux = JSON.stringify(travaux) // On convertit les données au format JSON string (chaîne de caractères)
+
+    //** Stockage des informations dans le localStorage **//
+    window.localStorage.setItem("travaux", valeurTravaux) // clé-valeur
+} else {
+    //** S'il y avait bien des données dans le localStorage, on reconstruit les données en mémoire **//
+    travaux = JSON.parse(travaux) // Analyse du JSON pour reconstruire les valeurs ou objets décrits par la chaîne de caractère
+}
+
+//*************** Déclaration des fonctions ***************//
 
 /*
 ** Déclaration de la fonction qui génère dynamiquement tout le contenu de la galerie des projets
@@ -79,13 +95,11 @@ function gererBoutonsCategorie(travaux, listeCategories) {
         btnCategorie[i].addEventListener("click", (event) => { // Ajout d'un écouteur sur l'évènement "click"
 
             let btnCategorieClick = event.target.value // Sélection du bouton qui a déclenché l'évènement (bouton sur lequel l'utilisateur a cliqué)
-            console.log(btnCategorieClick) // Vérif
 
             for (let index = 0; index < listeCategories.length; index++) {
                 if (btnCategorieClick === listeCategories[index].name) {
 
                     let categorieChoisie = travaux.filter(travaux => travaux.category.name === listeCategories[index].name)
-                    console.log(categorieChoisie) // Vérif
                     document.querySelector(".gallery").innerHTML = ""
                     afficherGalerie(categorieChoisie)
 
@@ -96,6 +110,7 @@ function gererBoutonsCategorie(travaux, listeCategories) {
 }
 
 
+//*************** Exécution du script ***************//
 
 /*
 ** Premier affichage de la galerie de travaux
@@ -103,14 +118,13 @@ function gererBoutonsCategorie(travaux, listeCategories) {
 afficherGalerie(travaux)
 
 /* 
-** Récupération des catégories en supprimant les doublons
+** Récupération des catégories de travaux, en supprimant les doublons
 */
 let categories = new Map() // Création d'un nouvel objet Map, pour y stocker des paires de clé-valeur en mémorisant l'ordre d'insertion.
 travaux.forEach( // méthode forEach pour exécuter une fonction une fois pour chaque élément du tableau "travaux"
     projet => categories.set(projet.category.id, projet.category) // fonction lambda + set(key, value) appliqué à la structure Map pour y stocker des données
 )
 let listeCategories = Array.from(categories.values()) // Constitution d'un tableau à partir des values
-console.log(listeCategories) // Vérif
 
 /*
 ** Génération dynamique du menu de catégories
