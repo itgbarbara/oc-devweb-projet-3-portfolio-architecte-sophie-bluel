73d6128a8204fb380bb8e9e1301820e7a64ef3d2
itgbarbara <itgbarbara@gmail.com>
2024-05-20 12:32:10 +0200

    Renaming JS files

--- FrontEnd/scripts/homepage_portfolio.js
+++ FrontEnd/scripts/homepage_portfolio.js
@@ -3,24 +3,16 @@
 */
 import { afficherGalerie, genererBoutonsCategorie, gererBoutonsCategorie } from "./script.js"
 
-
 /*
 ** Récupération des données depuis le localStorage ou l'API HTTP
 */
-//** Récupération des pièces éventuellement stockées dans le localStorage **//
 let travaux = window.localStorage.getItem("travaux")
 if (travaux === null) { // peut aussi s'écrire if (!travaux)
-    //** S'il n'y a rien dans le localStorage, récupération des travaux depuis l'API **//
     const reponse = await fetch("http://localhost:5678/api/works") // Envoi d'une requête GET pour récupérer les données de l'API (route : GET /works) + stockage du résultat dans une constante
     const travaux = await reponse.json() // Désérialisation du JSON et stockage de la liste d'objets (= travaux) obtenue dans la variable "travaux"
-
-    //** Transformation des travaux en JSON pour les stocker dans le localStorage **//
     const valeurTravaux = JSON.stringify(travaux) // On convertit les données au format JSON string (chaîne de caractères)
-
-    //** Stockage des informations dans le localStorage **//
-    window.localStorage.setItem("travaux", valeurTravaux) // clé-valeur
+    window.localStorage.setItem("travaux", valeurTravaux) // Stockage des informations dans le localStorage (clé, valeur)
 } else {
-    //** S'il y avait bien des données dans le localStorage, on reconstruit les données en mémoire **//
     travaux = JSON.parse(travaux) // Analyse du JSON pour reconstruire les données (valeurs ou objets) décrites par la chaîne de caractère
 }
 
