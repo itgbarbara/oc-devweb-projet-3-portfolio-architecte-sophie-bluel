bb635ea2d3cc946f1887dfa99ffc1f2aaaa3601e
itgbarbara <itgbarbara@gmail.com>
2024-05-20 10:34:28 +0200

    Adding edition_mode.js

---
+++ FrontEnd/scripts/edition_mode.js
@@ -0,0 +1,10 @@
+/*
+** Import des fonctions
+*/
+import { activerModeEdition } from "./script.js"
+
+/*
+** Récupération des données depuis le localStorage ou l'API HTTP
+*/
+
+// => Récupérer le token stocké dans le local storage, s'il existe, activer le mode édition, sinon ne rien faire
