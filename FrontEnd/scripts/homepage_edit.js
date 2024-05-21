989c6b5bb3a51cfb2e3daf67149b1852850b5363
itgbarbara <itgbarbara@gmail.com>
2024-05-21 18:07:51 +0200

    Removing a useless variable

--- FrontEnd/scripts/homepage_edit.js
+++ FrontEnd/scripts/homepage_edit.js
@@ -10,10 +10,10 @@ import { activerModeEdition, deconnecterUtilisateur, desactiverModeEdition } fro
 //** Récupération du token éventuellement stockées dans le localStorage **//
 let storedToken = localStorage.getItem("token")
 if (storedToken !== null) { // si token existe
-    const token = JSON.parse(storedToken) // Reconstruction des données
-    activerModeEdition()
+    JSON.parse(storedToken) // Reconstruction des données
+    activerModeEdition() // Affichage des éléments liés à l'édition
 } else {
-    desactiverModeEdition()
+    desactiverModeEdition() // Retrait des éléments liés à l'édition
 }
 
 deconnecterUtilisateur()
