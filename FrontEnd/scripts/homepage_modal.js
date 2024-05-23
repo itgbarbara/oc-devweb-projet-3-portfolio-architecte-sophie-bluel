1835ac975e5e739b399393e9258cd2b93a42544b
itgbarbara <itgbarbara@gmail.com>
2024-05-23 17:19:19 +0200

    Calling functions to open modal

--- FrontEnd/scripts/homepage_modal.js
+++ FrontEnd/scripts/homepage_modal.js
@@ -1,11 +1,12 @@
 /*
 ** Import des fonctions
 */
-import { afficherGalerieModale, listerCategories, selectionnerCategorie } from "./script.js"
+import { afficherGalerieModale, listerCategories, selectionnerCategorie, ouvrirModale } from "./script.js"
 
 /*
 ** Récupération des données depuis le localStorage ou l'API HTTP
 */
+//** Récupération de la liste de travaux **//
 let travaux = window.localStorage.getItem("travaux")
 if (travaux === null) { // peut aussi s'écrire if (!travaux)
     const reponse = await fetch("http://localhost:5678/api/works") // Envoi d'une requête GET pour récupérer les données de l'API (route : GET /works) + stockage du résultat dans une constante
@@ -16,53 +17,29 @@ if (travaux === null) { // peut aussi s'écrire if (!travaux)
     travaux = JSON.parse(travaux) // Analyse du JSON pour reconstruire les données (valeurs ou objets) décrites par la chaîne de caractère
 }
 
+//** Récupération du token éventuellement stockées dans le localStorage **//
+let storedToken = localStorage.getItem("token")
+if (storedToken !== null) { // si token existe
+    let token = JSON.parse(storedToken) // Reconstruction des données
+    console.log(token)
+}
+
+
 
 //*************** Exécution du script ***************//
 
 
-/*
-** Affichage de la galerie de travaux
-*/
-afficherGalerieModale(travaux)
+let modal = null // Définition d'une variable globale "modal" qui est nulle par défaut
 
-let listeCategories = listerCategories(travaux)
+//** Ouvrir la modale **//
+document.querySelectorAll(".js-open-modal").forEach(btnModifier => {
+    btnModifier.addEventListener("click", ouvrirModale)
+})
 
-selectionnerCategorie(listeCategories)
+//** Affichage des données de l'API **//
+    /* Vue 1 */
+    afficherGalerieModale(travaux)
 
-
-
-
-// let modal = null
-
-// function openModal(event) {
-//     event.preventDefault()
-//     const target = document.querySelector(event.target.getAttribute("href"))
-//     target.style.display = null
-//     target.setAttribute("aria-hidden", "false")
-//     target.setAttribute("aria-modal", "true")
-//     modal = target
-//     modal.addEventListener("click", closeModal)
-//     modal.querySelector(".js-close-modal").addEventListener("click", closeModal)
-//     modal.querySelector(".js-stop-propagation").addEventListener("click", stopPropagation)
-// }
-
-// function closeModal(event) {
-//     if (modal === null) return
-//     event.preventDefault()
-//     modal.style.display = "none"
-//     modal.setAttribute("aria-hidden", "true")
-//     modal.removeAttribute("aria-modal")
-//     modal.removeEventListener("click", closeModal)
-//     modal.querySelector(".js-close-modal").removeEventListener("click", closeModal)
-//     modal.querySelector(".js-stop-propagation").removeEventListener("click", stopPropagation)
-//     modal = null
-// }
-
-// function stopPropagation(event) {
-//     event.stopPropagation() // Permet d'éviter la propagation de l'évènement vers les parents, pour empêcher que la modale se ferme quand on clique dans le wrapper
-// }
-
-
-// document.querySelectorAll(".js-open-modal").forEach(a => {
-//     a.addEventListener("click", openModal)
-// })
+    /* Vue 2 */
+    let listeCategories = listerCategories(travaux)
+    selectionnerCategorie(listeCategories)
