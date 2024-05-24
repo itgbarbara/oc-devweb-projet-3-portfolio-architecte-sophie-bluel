cd35221a135421c61a562f333afeb02425e6a681
itgbarbara <itgbarbara@gmail.com>
2024-05-24 18:35:50 +0200

    Getting token from locatStorage + reorganization of the script

--- FrontEnd/scripts/homepage_portfolio.js
+++ FrontEnd/scripts/homepage_portfolio.js
@@ -3,9 +3,10 @@
 */
 import { afficherGalerie, listerCategories, genererBoutonsCategorie, gererBoutonsCategorie } from "./script.js"
 
-/*
-** Récupération des données depuis le localStorage ou l'API HTTP
-*/
+
+//*************** Récupération des données depuis le localStorage ou l'API HTTP ***************//
+
+//** Récupération de la liste de travaux **//
 let travaux = window.localStorage.getItem("travaux")
 if (travaux === null) { // peut aussi s'écrire if (!travaux)
     const reponse = await fetch("http://localhost:5678/api/works")
@@ -16,25 +17,23 @@ if (travaux === null) { // peut aussi s'écrire if (!travaux)
     travaux = JSON.parse(travaux)
 }
 
+//** Récupération du token éventuellement stockées dans le localStorage **//
+let token = localStorage.getItem("token")
+let isLogedIn = false
+if (token !== null) { // si token existe
+    token = JSON.parse(token) // Reconstruction des données
+    isLogedIn = true
+}
+
 
 //*************** Exécution du script ***************//
 
-/*
-** Premier affichage de la galerie de travaux
-*/
+//** Premier affichage de la galerie de travaux **//
 afficherGalerie(travaux)
 
-/* 
-** Récupération des catégories de travaux, en supprimant les doublons
-*/
+//** Génération dynamique du menu de catégories **//
 let listeCategories = listerCategories(travaux)
-
-/*
-** Génération dynamique du menu de catégories
-*/
 genererBoutonsCategorie(listeCategories)
 
-/*
-** Gestion du fonctionnement des boutons du menu de catégories
-*/
+//** Gestion du fonctionnement des boutons du menu de catégories **//
 gererBoutonsCategorie(travaux, listeCategories)
