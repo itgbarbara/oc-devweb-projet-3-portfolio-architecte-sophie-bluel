dc60ff16d254cc61a365d2a435f3706d0f01be40
itgbarbara <itgbarbara@gmail.com>
2024-05-24 15:38:04 +0200

    Renaming the variable "storedToken" in "token"

--- FrontEnd/scripts/homepage_edit.js
+++ FrontEnd/scripts/homepage_edit.js
@@ -8,9 +8,11 @@ import { activerModeEdition, deconnecterUtilisateur, desactiverModeEdition } fro
 */
 
 //** Récupération du token éventuellement stockées dans le localStorage **//
-let storedToken = localStorage.getItem("token")
-if (storedToken !== null) { // si token existe
-    JSON.parse(storedToken) // Reconstruction des données
+let token = localStorage.getItem("token")
+let isLogedIn = false
+if (token !== null) { // si token existe
+    token = JSON.parse(token)
+    isLogedIn = true
     activerModeEdition() // Affichage des éléments liés à l'édition
 } else {
     desactiverModeEdition() // Retrait des éléments liés à l'édition
