ae088492845ea1739349abd39e68fc01a2b21870
itgbarbara <itgbarbara@gmail.com>
2024-05-24 15:37:41 +0200

    Deleting comments

--- FrontEnd/scripts/homepage_portfolio.js
+++ FrontEnd/scripts/homepage_portfolio.js
@@ -8,12 +8,12 @@ import { afficherGalerie, listerCategories, genererBoutonsCategorie, gererBouton
 */
 let travaux = window.localStorage.getItem("travaux")
 if (travaux === null) { // peut aussi s'écrire if (!travaux)
-    const reponse = await fetch("http://localhost:5678/api/works") // Envoi d'une requête GET pour récupérer les données de l'API (route : GET /works) + stockage du résultat dans une constante
-    const travaux = await reponse.json() // Désérialisation du JSON et stockage de la liste d'objets (= travaux) obtenue dans la variable "travaux"
-    const valeurTravaux = JSON.stringify(travaux) // On convertit les données au format JSON string (chaîne de caractères)
-    window.localStorage.setItem("travaux", valeurTravaux) // Stockage des informations dans le localStorage (clé, valeur)
+    const reponse = await fetch("http://localhost:5678/api/works")
+    const travaux = await reponse.json()
+    const valeurTravaux = JSON.stringify(travaux)
+    window.localStorage.setItem("travaux", valeurTravaux)
 } else {
-    travaux = JSON.parse(travaux) // Analyse du JSON pour reconstruire les données (valeurs ou objets) décrites par la chaîne de caractère
+    travaux = JSON.parse(travaux)
 }
 
 
