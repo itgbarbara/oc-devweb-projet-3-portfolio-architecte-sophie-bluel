7c1f396336361dd909ea5812a4816be16acd8aa8
itgbarbara <itgbarbara@gmail.com>
2024-05-24 18:35:20 +0200

    Fetching works from the API#	modified:   FrontEnd/scripts/homepage_portfolio.js

--- FrontEnd/scripts/homepage_edit.js
+++ FrontEnd/scripts/homepage_edit.js
@@ -3,9 +3,19 @@
 */
 import { activerModeEdition, deconnecterUtilisateur, desactiverModeEdition } from "./script.js"
 
-/*
-** Récupération des données depuis le localStorage ou l'API HTTP
-*/
+
+//*************** Récupération des données depuis le localStorage ou l'API HTTP ***************//
+
+//** Récupération de la liste de travaux **//
+let travaux = window.localStorage.getItem("travaux")
+if (travaux === null) { // peut aussi s'écrire if (!travaux)
+    const reponse = await fetch("http://localhost:5678/api/works")
+    const travaux = await reponse.json()
+    const valeurTravaux = JSON.stringify(travaux)
+    window.localStorage.setItem("travaux", valeurTravaux)
+} else {
+    travaux = JSON.parse(travaux)
+}
 
 //** Récupération du token éventuellement stockées dans le localStorage **//
 let token = localStorage.getItem("token")
